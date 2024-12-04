2a56857798383aa952cc51f69920f1b46922525f
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-12-05 01:18:19 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_bloc_practice_new/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_bloc_practice_new
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/data/wishlist.dart
@@ -0,0 +1,3 @@
+
+
+// List<ProductDataModel> wishlistItems = [];
